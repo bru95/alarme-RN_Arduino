404d44857958c8a9654fb4b61349729334b41604
Bruna Lusa Balbinot <brunalbalbinot@gmail.com>
2020-10-18 17:39:36 -0300

    ajustes layout e estrutura

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './src/pages/HomePage';
-import AlertPage from './src/pages/AlertPage';
-import Icon from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 Icon.loadFont();
 
 const HomeStack = createStackNavigator();
@@ -15,16 +14,6 @@ function HomeStackScreen() {
     <HomeStack.Navigator>
       <HomeStack.Screen name="Home" component={HomePage} />
     </HomeStack.Navigator>
-  );
-}
-
-const SettingsStack = createStackNavigator();
-
-function SettingsStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Alertas" component={AlertPage} />
-    </SettingsStack.Navigator>
   );
 }
 
@@ -42,11 +31,8 @@ export default function App() {
               case 'Home':
                 iconName = 'home';
                 break;
-              case 'Alertas':
-                iconName = 'bell';
-                break;
               default:
-                iconName = 'circle';
+                iconName = 'code';
                 break;
             }
 
@@ -59,40 +45,7 @@ export default function App() {
         }}
       >
         <Tab.Screen name="Home" component={HomeStackScreen} Icon />
-        <Tab.Screen name="Alertas" component={SettingsStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-/*export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
-            }
-
-            // You can return any component that you like here!
-            return <Icon name="add" size={20} color="#000" />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}*/
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Switch, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 Icon.loadFont();
 
 export default class HomePage extends React.Component {
@@ -32,11 +32,18 @@ export default class HomePage extends React.Component {
     render() {
         const { id, name, active } = this.state.device;
 
+        let text;
+        if (active) {
+            text = 'Desarmar alarme';
+        } else {
+            text = 'Armar alarme';
+        }
+
         return (
             <View key={id} style={styles.container}>
                 <View style={styles.content}>
 
-                    <Icon name='cpu' size={70} color='#777' />
+                    <Icon name='settings-remote' size={70} color='#777' />
                     <Text style={{ marginTop: 10 }} >{name}</Text>
                     <Switch
                         trackColor={{ false: "#8B0000", true: "#008000" }}
@@ -46,6 +53,7 @@ export default class HomePage extends React.Component {
                         onValueChange={this.toggleSwitch}
                         style={{ marginTop: 10 }}
                     />
+                    <Text style={{ marginTop: 10, fontStyle: 'italic' }} >{text}</Text>
                 </View>
             </View>
         )
