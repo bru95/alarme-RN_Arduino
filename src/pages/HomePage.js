1939e9a77fe73d73cebc09fd13ea6835cfa65d68
Bruna Lusa Balbinot <brunalbalbinot@gmail.com>
2020-11-03 21:56:53 -0300

    ajustes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Switch, Text } from 'react-native';
+import { StyleSheet, View, Switch, Text, NativeModules, NativeEventEmitter } from 'react-native';
+import BleManager from 'react-native-ble-manager';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 Icon.loadFont();
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 export default class HomePage extends React.Component {
     constructor(props) {
@@ -11,23 +15,73 @@ export default class HomePage extends React.Component {
             device:
             {
                 id: 1,
-                name: 'Device I',
-                active: true
-            }
+                name: 'Procurando ...'
+            },
+            peripherals: new Map()
         }
+    }
+
+    componentDidMount() {
+        const DEVICE_NAME = "HC-06";
+        BleManager.start({ showAlert: false })
+
+        /*this.handlerDiscover = bleManagerEmitter.addListener(
+            'BleManagerDiscoverPeripheral',
+            this.handleDiscoverPeripheral
+        );
+
+        this.handlerStop = bleManagerEmitter.addListener(
+            'BleManagerStopScan',
+            this.handleStopScan
+        );
+
+        this.scanForDevices(); // I chose to start scanning for devices here*/
+
+        BleManager.getBondedPeripherals([]).then((bondedPeripheralsArray) => {
+            bondedPeripheralsArray.map(device => {
+                if (device.name == DEVICE_NAME) {
+                    this.setState({ device: { name: device.name, id: device.id, active: false } });
+                }
+            })
+        });
+    }
+
+    scanForDevices() {
+        BleManager.scan([], 15);
+    }
+
+    handleDiscoverPeripheral = (peripheral) => {
+        const { peripherals } = this.state;
+
+        if (peripheral.id == "98:D3:32:31:15:05") {
+            peripherals.set(peripheral.id, "HC-06");
+        }
+        this.setState({ peripherals });
+    };
+
+    handleStopScan = () => {
+        console.log('Scan is stopped. Devices: ', this.state.peripherals);
     }
 
     toggleSwitch = () => {
         var active = !this.state.device.active;
+        let device = this.state.device;
+        device.active = active;
         this.setState({
-            device:
-            {
-                id: 1,
-                name: 'Device I',
-                active: active
-            }
+            device: device
         });
+        //send bool to HC-06
+        this.sendData(active, device);
     };
+
+    sendData(data, device) {
+        console.log("Conectando ... " + device.id);
+        BleManager.connect(device.id).then(() => {
+            console.log('Connected to ' + device.name);
+        }).catch((error) => {
+            console.log('Connection error', error);
+        });
+    }
 
     render() {
         const { id, name, active } = this.state.device;
